31ba1e0b4d77207a044edb1f7cf6ea8fbe0b26e0
Alan Turing <trond@blacktie-llc.com>
2019-08-23 11:31:31 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+var userInput = document.querySelector('.ask-box');
+var possibleAnswers = document.querySelector();
+
+submitButton.addEventListener('click', createRandomAnswer);
+
+function askQuestion() {
+ userInput.innerText = possibleAnswers.value;
+ console.log('ask-box')
+}
